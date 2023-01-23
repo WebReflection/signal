492b12614757be7e3b671b3ee6ca4f72d185b030
webreflection <andrea.giammarchi@gmail.com>
2023-01-23 23:24:12 +0100

    v1 - avoid dangling leaks on signals

--- esm/index.js
+++ esm/index.js
@@ -1,39 +1,87 @@
-/** (c) Andrea Giammarchi - ISC */
+/*! (c) Andrea Giammarchi */
+
+let batches = null;
+
+/**
+ * Invoke a callback that updates many signals and runs effects only after.
+ * @type {(fn:() => void) => void}
+ */
+export const batch = fn => {
+  let effects = batches;
+  if (!effects) batches = new Set;
+  try { fn() }
+  finally {
+    if (!effects) {
+      [batches, effects] = [null, batches];
+      for (const effect of effects) effect._();
+    }
+  }
+};
+
+const cleared = self => {
+  const entries = [...self];
+  self.clear();
+  return entries;
+};
+
+class Effect extends Set {
+  constructor(_) { super()._ = _ }
+  dispose() {
+    for (const entry of cleared(this)) {
+      entry.delete(this);
+      entry.dispose?.();
+    }
+  }
+}
+
+let current = null;
+
+/**
+ * Invokes a function when any of its internal signals or computed values change.
+ * Returns a `dispose` callback.
+ * @template T
+ * @type {<T>(fn: (v?: T) => T | undefined, value?: T) => () => void}
+ */
+export const effect = (fn, value) => {
+  const fx = new Effect(() => {
+    const prev = current;
+    current = fx;
+    try { value = fn(value) }
+    finally { current = prev; }
+  });
+  if (current) current.add(fx);
+  return fx._(), () => fx.dispose();
+};
 
 /**
  * A signal with a value property also exposed via toJSON, toString and valueOf.
  * @template T
  */
-export class Signal {
-  #value;
-  #effects = [];
+export class Signal extends Set {
 
   /** @param {T} value the value carried through the signal */
-  constructor(value) { this.#value = value }
+  constructor(_) { super()._ = _ }
 
   /** @returns {T} */
   get value() {
-    const {length} = effects;
-    if (length)
-      this.#effects.push(effects[length - 1]);
-    return this.#value;
+    if (current) current.add(this.add(current));
+    return this._;
   }
 
   /** @param {T} value the new value carried through the signal */
-  set value(value) {
-    if (this.#value !== value) {
-      this.#value = value;
-      if (this.#effects.length) {
-        if (batches === effects)
-          dispatch(this.#effects.splice(0));
-        else
-          batches.push(...this.#effects.splice(0));
+  set value(_) {
+    if (this._ !== _) {
+      this._ = _;
+      const root = !batches;
+      for (const effect of cleared(this)) {
+        if (root) effect._();
+        else batches.add(effect);
       }
     }
   }
 
   // EXPLICIT NO SIDE EFFECT
-  peek() { return this.#value }
+  peek() { return this._ }
 
   // IMPLICIT SIDE EFFECT
   then(resolve) { resolve(this.value) }
@@ -41,6 +89,13 @@ export class Signal {
   valueOf() { return this.value }
   toString() { return String(this.value) }
 }
+
+/**
+ * Returns a writable Signal that side-effects whenever its value gets updated.
+ * @template T
+ * @type {<T>(value: T) => Signal<T>}
+ */
+export const signal = value => new Signal(value);
 
 /**
  * A read-only Signal extend that is invoked only when any of the internally
@@ -61,7 +116,7 @@ export class Computed extends Signal {
      * @type {() => void}
      */
     this.dispose = effect(() => {
-      super.value = fn(this.peek());
+      super.value = fn(this._);
     });
   }
 
@@ -73,70 +128,9 @@ export class Computed extends Signal {
 }
 
 /**
- * Invoke a callback that updates many signals and runs effects only after.
- * @type {(fn:() => void) => void}
- */
-export const batch = fn => {
-  const root = batches === effects;
-  if (root)
-    batches = [];
-  try { fn() }
-  finally {
-    if (root) {
-      const all = batches;
-      batches = effects;
-      dispatch(all);
-    }
-  }
-};
-
-/**
  * Returns a Computed signal that is invoked only when any of the internally
  * used signals, as in within the callback, is unknown or updated.
  * @template T
  * @type {<T>(fn: (v?: T) => T, value?: T) => Computed<T>}
  */
 export const computed = (fn, value) => new Computed(fn, value);
-
-/**
- * Invokes a function when any of its internal signals or computed values change.
- * Returns a `dispose` callback.
- * @template T
- * @type {<T>(fn: (v?: T) => T | undefined, value?: T) => () => void}
- */
-export const effect = (fn, value) => {
-  let active = true;
-  const fx = () => {
-    if (active) {
-      effects.push(fx);
-      try { value = fn(value) }
-      finally { effects.pop() }
-    }
-  };
-  const dispose = () => {
-    active = false;
-    for (const dispose of disposes.get(fx))
-      dispose();
-  };
-  disposes.set(fx, []);
-  const {length} = effects;
-  if (length)
-    disposes.get(effects[length - 1]).push(dispose);
-  return fx(), dispose;
-};
-
-/**
- * Returns a writable Signal that side-effects whenever its value gets updated.
- * @template T
- * @type {<T>(value: T) => Signal<T>}
- */
-export const signal = value => new Signal(value);
-
-const effects = [];
-const disposes = new WeakMap;
-const dispatch = effects => {
-  for (const effect of new Set(effects))
-    effect();
-};
-
-let batches = effects;
